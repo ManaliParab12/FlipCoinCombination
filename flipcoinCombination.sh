7db79006cd9444f664e95e11aae3c66449b61a2e
ManaliParab12 <manaliparab10@gmail.com>
2020-10-15 08:16:22 +0530

    single coin combination stored in dictionary and determined the percentage

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -x
 
+declare -A flip_coin
+
 echo "Welcome to Flip Coin Combination"
+H=0
+T=0
+read -p "Enter the number of flip : " n
 
-coin=$((RANDOM%2))
+for (( i=0; i<n; i++ ))
+do
+   single=$((RANDOM%2))
 
-if [ $coin -eq 1 ]
-then
-    echo "You got Heads"
-else
-    echo "You got Tails"
-fi
+   if [ $single -eq 1 ]
+   then
+       flip_coin[H]="$((flip_coin[H]+1))"
+   else
+       flip_coin[T]="$((flip_coin[T]+1))"
+   fi
+done
+
+HPercent=`echo $((${flip_coin[H]} * 100 / $n))`
+TPercent=`echo $((${flip_coin[T]} * 100 / $n))`
+
+echo "Percentage of Head " $HPercent
+echo "Percentage of Tail" $TPercent
